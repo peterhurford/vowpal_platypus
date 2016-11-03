b595810c5bbcd8478d61a939afa143cfdddedda5
peterhurford <peter@peterhurford.com>
2016-11-03 10:15:39 -0500

    remove import local

--- vowpal_porpoise/vp_utils.py
+++ vowpal_porpoise/vp_utils.py
@@ -1,18 +1,16 @@
+import os
+import sys
+
 def safe_remove(f):
-    import os
     try:
         os.remove(f)
     except OSError:
         pass
 
-"""
-Basic logger functionality; replace this with a real logger of your choice
-"""
-import imp
-import sys
-
-
 class VPLogger:
+    """
+    Basic logger functionality; replace this with a real logger of your choice
+    """
     def debug(self, s):
         print '[DEBUG] %s' % s
 
@@ -24,15 +22,3 @@ class VPLogger:
 
     def error(self, s):
         print '[ERROR] %s' % s
-
-
-def import_non_local(name, custom_name=None):
-    """Import when you have conflicting names"""
-    custom_name = custom_name or name
-
-    f, pathname, desc = imp.find_module(name, sys.path[1:])
-    module = imp.load_module(custom_name, f, pathname, desc)
-    if f:
-      f.close()
-
-    return module
