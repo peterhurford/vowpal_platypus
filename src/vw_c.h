ed9d81f14cc1e0de556149fd7882404f6d48bb2c
peterhurford <peter@peterhurford.com>
2016-12-06 14:21:45 -0600

    No src folder

--- src/vw_c.h
+++
@@ -1,8 +0,0 @@
-#ifndef VW_C_H
-#define VW_C_H
-
-void initialize(char* params);
-void finish();
-float learn(char* example);
-
-#endif
--- src/vw_c.pxd
+++
@@ -1,4 +0,0 @@
-cdef extern from "vw_c.h":
-    void initialize(char*)
-    void finish()
-    float learn(char*)
