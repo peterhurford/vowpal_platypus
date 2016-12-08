347b64c1c5411e265dedb2efc46f180b99ce7780
peterhurford <peter@peterhurford.com>
2016-12-08 11:24:47 -0600

    Add a CHANGELOG and re-version

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+## v1.0.1
+
+* Fix conflict between spanning port and daemon port.
+* Allow VP to work on one core.
+* Fix bug where `quiet` would not actually quiet model output.
+
+## v1.0
+
+* Initial pre-release.
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'vowpal_platypus',
-    version = '0.5',
+    version = '1.0.1',
     author = 'Peter Hurford',
     author_email = 'peter@peterhurford.com',
     description='Lightweight vowpal wabbit wrapper',
