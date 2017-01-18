d687fa3a1d3ba0e8ba7b832a3ed8e29f08450f18
peterhurford <peter@peterhurford.com>
2017-01-18 14:28:27 -0600

    fix params hash copy bug

--- vowpal_platypus/vw.py
+++ vowpal_platypus/vw.py
@@ -470,20 +470,21 @@ class VW:
 
 
 def vw_model(model_params, node=False):
+    params = model_params.copy()
     if node is not False:
-        assert model_params.get('cores'), '`cores` parameter must specify the total number of cores to make a multi-core model.'
+        assert params.get('cores'), '`cores` parameter must specify the total number of cores to make a multi-core model.'
         multicore_params = {
-            'total': model_params['cores'],
+            'total': params['cores'],
             'node': node,
             'holdout_off': True,
             'span_server': 'localhost'
         }
-        model_params.update(multicore_params)
-        if not model_params.get('unique_id'):
-            model_params['unique_id'] = 0
-    if model_params.get('cores'):
-        model_params.pop('cores')
-    return VW(model_params)
+        params.update(multicore_params)
+        if not params.get('unique_id'):
+            params['unique_id'] = 0
+    if params.get('cores'):
+        params.pop('cores')
+    return VW(params)
 
 def model(model_params):
     cores = model_params.get('cores')
