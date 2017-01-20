383d1a7a3edf8085997afeb1a8fc64a180ef8ef4
peterhurford <peter@peterhurford.com>
2017-01-20 11:29:08 -0600

    consistent treatment of results tuple in evaluation

--- vowpal_platypus/evaluation.py
+++ vowpal_platypus/evaluation.py
@@ -1,15 +1,15 @@
 from math import sqrt, log
 
 def log_loss(results):
-     predicted = [min([max([x, 1e-15]), 1-1e-15]) for x in map(lambda x: float(x['predicted']), results)]
-     target = [min([max([x, 1e-15]), 1-1e-15]) for x in map(lambda x: float(x['actual']), results)]
+     predicted = [min([max([x, 1e-15]), 1-1e-15]) for x in map(lambda x: float(x[0]), results)]
+     target = [min([max([x, 1e-15]), 1-1e-15]) for x in map(lambda x: float(x[1]), results)]
      return -(1.0 / len(target)) * sum([target[i] * log(predicted[i]) + (1.0 - target[i]) * log(1.0 - predicted[i]) for i in xrange(len(target))])
 
 def rmse(results):
-    return (sum(map(lambda x: (x['actual'] - x['predicted']) ** 2, results)) / len(results)) ** 0.5
+    return (sum(map(lambda x: (x[1] - x[0]) ** 2, results)) / len(results)) ** 0.5
 
 def percent_correct(results):
-    return sum(map(lambda x: x['actual'] == (-1 if x['predicted'] < 0 else 1), results)) / float(len(results)) * 100
+    return sum(map(lambda x: x[1] == (-1 if x[0] < 0 else 1), results)) / float(len(results)) * 100
 
 def precision(results):
     true_positives = sum(map(lambda x: x[0] >= 0.5, filter(lambda x: x[1] == 1, results)))
