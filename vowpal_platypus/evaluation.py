8ae325cae8dbca9c4514b40fa2016a1bd9dd92ae
peterhurford <peter@peterhurford.com>
2017-01-20 17:34:31 -0600

    fix RMSE

--- vowpal_platypus/evaluation.py
+++ vowpal_platypus/evaluation.py
@@ -6,7 +6,7 @@ def log_loss(results):
      return -(1.0 / len(target)) * sum([target[i] * log(predicted[i]) + (1.0 - target[i]) * log(1.0 - predicted[i]) for i in xrange(len(target))])
 
 def rmse(results):
-    return (sum(map(lambda x: (x[1] - x[0]) ** 2, results)) / len(results)) ** 0.5
+    return (sum(map(lambda x: (x[1] - x[0]) ** 2, results)) / float(len(results))) ** 0.5
 
 def percent_correct(results):
     return sum(map(lambda x: x[1] == (0 if x[0] < 0.5 else 1), results)) / float(len(results)) * 100
