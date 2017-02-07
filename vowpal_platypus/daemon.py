aeb6004172cfd320ce66ab753d4ec4cd36b1b33f
peterhurford <peter@peterhurford.com>
2017-02-06 18:19:30 -0600

    Fixed bug in daemon

--- vowpal_platypus/daemon.py
+++ vowpal_platypus/daemon.py
@@ -1,21 +1,25 @@
 from internal import netcat
+from utils import vw_hash_to_vw_str
 from vw import VW
 
-def daemon(model):
+def daemon(model, port=4040):
     if model.params.get('node'):
-        port = model.params['node'] + 4040
-    else:
-        port = 4040
+        port = model.params['node'] + port
     daemon_model = VW({'name': model.handle,
                        'daemon': True,
                        'old_model': model.get_model_file(),
                        'holdout_off': True,
-                       'quiet': True if model.params.get('quiet') else False,
+                       'quiet': True,
                        'port': port,
                        'num_children': 2})
+    daemon_model.start_predicting()
     return daemon_model
 
 def daemon_predict(daemon, content, quiet=False):
+    if len(content) == 1:
+        content = [content]
+    if isinstance(content[0], dict):
+        content = '\n'.join(map(vw_hash_to_vw_str, content))
     return netcat('localhost',
                   port=daemon.params['port'],
                   content=content,
--- vowpal_platypus/internal.py
+++ vowpal_platypus/internal.py
@@ -32,7 +32,7 @@ class VPLogger:
         print '[ERROR] %s' % s
 
 
-@retry(wait_random_min=1000, wait_random_max=2000, stop_max_attempt_number=40)
+@retry(wait_random_min=1000, wait_random_max=2000, stop_max_attempt_number=4)
 def netcat(hostname, port, content, quiet=False):
     if not quiet:
         print('Connecting to port {}'.format(port))
