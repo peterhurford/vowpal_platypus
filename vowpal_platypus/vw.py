abdd5094664b98ad8b50a7c072fa8cb1e02ee2e5
peterhurford <peter@peterhurford.com>
2017-02-07 20:13:31 -0600

    TODO

--- vowpal_platypus/vw.py
+++ vowpal_platypus/vw.py
@@ -478,7 +478,7 @@ def run_parallel(vw_models, core_fn):
                 os.system('killall vw')
                 os.system('killall spanning_tree')
                 raise e
-        results = pool.map(run_fn, vw_models)
+        results = pool.map(run_fn, vw_models) # TODO: Integrate into `run`
         os.system('killall spanning_tree')
         return results
     else:
